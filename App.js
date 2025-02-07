e234441fc0984fae96dbe7fbc82832b056dadf62
Harman Kaur <harmankaur022001@gmail.com>
2025-02-07 17:46:29 -0600

    Imported lineargradient and blurview from expo.

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react'
+import { LinearGradient } from 'expo-linear-gradient';
+import { BlurView } from 'expo-blur';
 
 export default function App() {
   const [num1, setNum1] = useState('');
